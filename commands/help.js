8c6dd16c671d1be47d2989a73abb042843e6fc48
BlaGro <hyper.miniaturki@gmail.com>
2018-09-03 17:29:56 +0200

    Zmiany 1

--- commands/help.js
+++ commands/help.js
@@ -3,8 +3,8 @@ const Discord = require("discord.js");
 module.exports.run = async(bot, message, args) => {
   let embed = new Discord.RichEmbed()
   .setDescription(`**Książka pomocnicza dla użytkownika ${message.author.username}!**`)
-  .addField("**Komendy z kategorii anime**", "**girl - pokazuje randomowe zdj z folderu bota :D**", "**boy - pokazuje randomowe zdj z folderu bota**")
-  .addField("**Komendy z kategorii zdj profilowe**", "**avatar - pokazuje twój avatar**")
+  .addFields("**Komendy z kategorii anime**", "**girl - pokazuje randomowe zdj z folderu bota :D**", "**boy - pokazuje randomowe zdj z folderu bota**")
+  .addFields("**Komendy z kategorii zdj profilowe**", "**avatar - pokazuje twój avatar**")
   message.channel.send(embed);
 }
 
