9c754013226397b8925b11d2f78c0efddf088eb8
BlaGro <hyper.miniaturki@gmail.com>
2018-09-06 16:51:22 +0200

    Zmiany 1

--- commands/girl.js
+++ commands/girl.js
@@ -4,7 +4,7 @@ module.exports.run = async(bot, message, args) => {
   let embed = new Discord.RichEmbed()
   number = 20;
   imageNumber = Math.floor (Math.random() * (number -1 + 1)) + 1;
-  message.channel.send("Oto randomowe zdj z folderu :D", {files: ["./girls/" + imageNumber + ".jpg"]} )
+  message.channel.send("**Oto randomowe zdj z folderu :D**", {files: ["./girls/" + imageNumber + ".jpg"]} )
 }
 
 module.exports.help = {
