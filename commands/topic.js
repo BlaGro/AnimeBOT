56a0c72ee7d35fe541064d4a5ba834f1d0e439c9
BlaGro <hyper.miniaturki@gmail.com>
2018-10-23 21:43:07 +0200

    Zmiany

--- commands/topic.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js")
-
-module.exports.run = async(bot, message, args) => {
-	message.channel.send(`Temat tego kanału to ${message.textChannel.topic}`)
-}
-
-module.exports.help {
-	name:"topic"
-}
